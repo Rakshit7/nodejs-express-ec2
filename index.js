138b76b81793fefbea52b6863ecf4405fe2ffa0a
Choudhary <rakshit.choudhary@accenture.com>
2023-08-11 18:47:31 +0530

    new route added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.use("/", (req, res) => {
+    res.json("Server is up and running 😎😎");
+})
 app.listen(5001, () => {
     console.log("Server is up and running 😎😎");
 })
